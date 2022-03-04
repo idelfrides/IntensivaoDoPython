5679c2f9f605729f2bde5a5c8ae6f9d71f0c85b8
idelfrides <idelfridesjorgepapai@gmail.com>
2022-03-04 13:50:11 -0300

    FIRST COMMIT: create the main file of the app

---
+++ main_app.py
@@ -0,0 +1,29 @@
+
+
+
+
+def run_app():
+
+    print(f'\n\n\n THIS IS INTENSIVAO DO PYTHON SHORT COURSE \n')
+
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    run_app()
