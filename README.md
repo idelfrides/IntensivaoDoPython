2b74140e4b20e3196476a5900559ffe9e82b017d
idelfrides <idelfridesjorgepapai@gmail.com>
2022-03-09 14:11:11 -0300

    aula 2: done

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Intensivão do python promovido pela Lira da Hashtag
 
 
-### AULA 1: AUTOMAÇÃO DE TAREFAS REPETITIVAS
+### AULA 1: AUTOMAÇÃO DE TAREFAS REPETITIVAS | 2020-03-07
+
 
     Tarefas do dia-a-dia do seu trabalho .
 
@@ -14,9 +15,22 @@ Intensivão do python promovido pela Lira da Hashtag
 
     CONTEÚDO:
 
-    #1 DAY 1 [ 2022-03-07 ]: automatizar tarefas repetitivas. Aplicação do dia trabalha/aplica os seguintes conhecimentos:
+    #1 DAY 1 : automatizar tarefas repetitivas. Aplicação do dia trabalha/aplica os seguintes conhecimentos:
 
     -- Acessar uma base de dados no drive
     -- Fazer download dessa base
     -- Realizar tratamento de dados com pandas
     -- Enviar e-mail para qualquer pessoa.
+
+
+### AULA 2: AUTOMAÇÃO DE TAREFAS REPETITIVAS | 2020-03-08
+
+
+
+    #2 | DAY 2: Análise de dados com python.
+    Aplicação do dia trabalha/aplica os seguintes conhecimentos:
+
+    - Extração de dados a partir de uma fonte
+    - Importação para o ambiente de programação de python usando pandas
+    - Tratamento de dados no ambiente de python com pandas
+    - Apresentar os resultados em um gráficos utilizando o package PLOTLY
---
+++ aula_2_data_analisys.py
@@ -0,0 +1,101 @@
+
+"""
+ESCREVER O PASSO-A-PASSO  EM PORTUGUÊS
+----------------------------------------
+
+passo 1: importar a base de dados para o python
+passo 2: visualisar as informaçoes da base de dados
+    objetivo 1: analisar e entender os dados existentes na base de dados
+    objetivo 2: descobrir os erros (problemas) existentes na sua base de dados
+passo 3: tratamento dos dados
+passo 4: análise inicial / análise global (abordagem: top-down )
+passo 5: análise detalhada ( buscar a causa/ a solução dos cancelamentos)
+
+"""
+
+# pyautogui -> automatiza o mouse, teclado e o seu tela
+# pypercli
+# sempre q foi trabalhar com base de dados, usar pandas
+# pandas, numpy, openpyxl
+
+# --------------------------------------------------------------------------
+
+
+import os
+import pdb
+import time
+import pyautogui
+import pyperclip
+import pandas as pd
+import plotly.express as px
+import glob
+
+pyautogui.PAUSE = 1
+
+
+def data_analisys():
+
+    # -----------------------------------------------------------------
+    # passo 1: importar a base de dados para o python
+    # -----------------------------------------------------------------
+
+    try:
+        telecom_content = pd.read_csv(r'/media/ijdev/DATA/PROFISSIONAL/IntensivaoDoPython/utils/telecom_users.csv')
+    except Exception as err:
+        print(f'EXCEPTION: {err}')
+        telecom_content = 'NO TELECOM CONTENT'
+
+    print(telecom_content)
+
+    # ------------------------------------------------------------
+    # passo 2: visualisar as informaçoes da base de dados
+    # ------------------------------------------------------------
+    # Na análise de dados, o q não te ajuda, irá te atrapalhar
+
+    # axis = 0 --> linha
+    # axis = 1 --> coluna
+
+    # -------------------------------------------------------------
+    # passo 3: tratamento dos dados
+    # -------------------------------------------------------------
+    # analisar se o python está lendo os dados de forma correta
+
+    telecom_content = telecom_content.drop('Unnamed: 0', axis=1)
+    telecom_content = telecom_content.dropna(axis=1, how='all')
+    telecom_content = telecom_content.dropna(axis=0, how='any')
+
+    telecom_content['TotalGasto'] = pd.to_numeric(
+        telecom_content['TotalGasto'], errors='coerce')
+
+
+    # ----------------------------------------------------------------------
+    # passo 4: análise inicial / análise global (abordagem: top-down )
+    # ----------------------------------------------------------------------
+
+    print(telecom_content['Churn'].value_counts())
+    print(telecom_content['Churn'].value_counts(normalize=True).map("{:.2}%".format))
+
+
+    # ----------------------------------------------------------------
+    # passo 5: análise detalhada
+    # (buscar a causa/ a solução dos cancelamentos)
+    # ----------------------------------------------------------------
+    #  por definicao, histograma aplica y --> cquantidade de clientes
+
+    for column in telecom_content.columns:
+
+        # etapa 1: criar o gráfico
+        chart = px.histogram(telecom_content, x=column, color='Churn')
+
+        # etapa 2: mostrar o gráfico
+        chart.show()
+        time.sleep(30)
+
+
+    # CONCLUSÕES:
+
+    '''
+    1 -
+
+
+    '''
